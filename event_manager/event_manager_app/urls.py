87c86108b0f80621c783de97e6092751a24b3690
Vihar <202001110@daiict.ac.in>
2022-01-18 22:12:58 +0530

    Created event manager app and a temporary home page

--- event_manager/event_manager_app/urls.py
+++ event_manager/event_manager_app/urls.py
@@ -13,11 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import include
-from django.contrib import admin
-from django.urls import path
+
+from django.urls.conf import path
+from event_manager_app import views
 
 urlpatterns = [
-    path('', include('event_manager_app.urls')),
-    path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
 ]
