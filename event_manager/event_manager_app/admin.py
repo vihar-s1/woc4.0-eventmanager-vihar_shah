71db26b92490349341cc81e3b044d34cb547cafc
Vihar <202001110@daiict.ac.in>
2022-01-20 19:56:36 +0530

    style.css and Participant model created
    style.css is directly applied to the base.html page and Participant Model is created and registered on django admin site.

--- event_manager/event_manager_app/admin.py
+++ event_manager/event_manager_app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from event_manager_app.models import Event
+from event_manager_app.models import *
 
 # Register your models here.
 admin.site.register(Event)
+admin.site.register(Participant)
--- event_manager/event_manager_app/models.py
+++ event_manager/event_manager_app/models.py
@@ -4,13 +4,21 @@ from django.db import models
 
 class Event(models.Model):
     eventName = models.CharField(max_length=32)
-    description = models.TextField
+    description = models.TextField()
     location = models.CharField(max_length=32)
-    startFrom = models.DateTimeField
-    endat = models.DateTimeField
-    registerby = models.DateTimeField
-    hostEmail = models.EmailField
+    startDate = models.DateTimeField()
+    endDate = models.DateTimeField()
+    registerby = models.DateTimeField()
+    hostEmail = models.EmailField()
     hostpwd = models.CharField(max_length=15)
     
     def __str__(self) -> str:
         return self.eventName + " organized by " + self.hostEmail
+
+class Participant(models.Model):
+    name = models.CharField(max_length=20)
+    contact = models.CharField(max_length=15)
+    email = models.EmailField()
+    #event = models.ForeignKey(Event)
+
+    
